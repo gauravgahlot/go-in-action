74e0e5aab02ce84d907dae558b0f3e2bd4ab336f
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2024-07-30 17:32:47 +0530

    Fix all errors, all errors on the WORLD!!!!
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

---
+++ lexer/lexer_test.go
@@ -0,0 +1,41 @@
+package lexer_test
+
+import (
+	"testing"
+
+	"go-monkey/lexer"
+	"go-monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+	}
+
+	lex := lexer.New(input)
+	for i, tt := range tests {
+		tok := lex.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
